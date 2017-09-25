92b5e8ad34dd8eff77b492f8a09acd695d0fb70b
Mustapha M Garba <mgarba88@gmail.com>
2017-09-25 14:53:50 +0100

    Updated gamestate object
    Everything from line 5 is a new piece of code defining the window objct mainly.

--- tetris.py
+++ tetris.py
@@ -6,4 +6,14 @@ import pygame
 
 from pygame.locals import *
 
-def
+def main():
+    pygame.init()
+    pygame.display.set_mode((400, 500))
+
+    while True:
+        for event in pygame.event.get_event():
+            if event.type == QUIT:
+                terminate()
+
+if __name__ == '__main__':
+    main()
